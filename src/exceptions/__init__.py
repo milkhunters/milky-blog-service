13e0dbc6f2777bae1fbe97fbd6dfd3c68d098738
JKearnsl <boyarshinovn@gmail.com>
2023-07-12 17:42:49 +0300

    Удалены лишние файлы

--- src/exceptions/__init__.py
+++
@@ -1,2 +0,0 @@
-from .http import HTTPException, http_exception_handler
-from .api import APIError, api_exception_handler
--- src/exceptions/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api_error import Error, ErrorAPIResponse
-from .error import ErrorResponse
-
-#  TODO: удалить неиспользуемые
