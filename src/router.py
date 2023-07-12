0b612baf6936877102ffaa3a93152a40b1870f44
JKearnsl <boyarshinovn@gmail.com>
2023-07-12 14:28:59 +0300

    Изменен метод регистрации корневого маршрута

--- src/router.py
+++ src/router.py
@@ -8,16 +8,16 @@ from controllers import email
 from controllers import blog
 from controllers import notifications
 
-from config import load_config
 
-config = load_config()
+def register_api_router(is_debug: bool) -> APIRouter:
+    root_api_router = APIRouter(prefix="/api/v1" if is_debug else "")
 
-root_api_router = APIRouter(prefix="/api/v1" if config.debug else "",)
+    root_api_router.include_router(admin.router, prefix="/admin", tags=["Admin"])
+    root_api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+    root_api_router.include_router(email.router, prefix="/email", tags=["Email"])
+    root_api_router.include_router(user.router, prefix="/user", tags=["User"])
+    root_api_router.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
+    root_api_router.include_router(blog.router, prefix="/blog", tags=["Blog"])
+    root_api_router.include_router(stats.router, prefix="", tags=["Stats"])
 
-root_api_router.include_router(admin.router, prefix="/admin", tags=["Admin"])
-root_api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
-root_api_router.include_router(email.router, prefix="/email", tags=["Email"])
-root_api_router.include_router(user.router, prefix="/user", tags=["User"])
-root_api_router.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
-root_api_router.include_router(blog.router, prefix="/blog", tags=["Blog"])
-root_api_router.include_router(stats.router, prefix="", tags=["Stats"])
+    return root_api_router
