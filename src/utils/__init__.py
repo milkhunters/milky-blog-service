9ad0ae5087d4c4e447d79693e0d79446393ea3d0
JKearnsl <boyarshinovn@gmail.com>
2023-07-14 21:20:03 +0300

    update

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,3 @@
 from .redis import RedisClient
-from .aiohttp_client import AiohttpClient
 from . import formators
 from . import validators
-from .password import get_hashed_password, verify_password
-from . import other
