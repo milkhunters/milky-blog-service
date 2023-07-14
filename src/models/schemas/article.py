d13174d0a7158145563dc0c91661ddeeded221ac
JKearnsl <boyarshinovn@gmail.com>
2023-07-14 21:31:15 +0300

    Переработан `article.py`: делегированы классы, изменены типы

--- src/models/schemas/article.py
+++ src/models/schemas/article.py
@@ -1,101 +1,61 @@
+import uuid
 from datetime import datetime
-from typing import Any, Optional, List
 
-from pydantic import BaseModel, validator
-from tortoise import fields
+from pydantic import BaseModel
+from src.models.state import ArticleState
+from .user import UserSmall
 
-from .user import User
-from models.state import ArticleState
+
+class ArticleTagItem(BaseModel):
+    id: uuid.UUID
+    title: str
 
 
 class Article(BaseModel):
-    """
-    Базовая модель статьи
-
-    """
-    id: int
+    id: uuid.UUID
     title: str
-    poster_url: Optional[str]
-    state: ArticleState  # TODO: перейти на enum
-    description: str
     content: str
-    tags: Optional[list['Tag']]
-    owner: User
-    create_time: datetime
-    update_time: Optional[datetime]
+    poster_url: str = None
+    tags: list[ArticleTagItem]
+    state: ArticleState
+    owner: UserSmall
 
-    @validator("*", pre=True, each_item=False)
-    def _tortoise_convert(cls, value):
-        # Computed fields
-        if callable(value):
-            return value()
-        # Convert ManyToManyRelation to list
-        if isinstance(value, (fields.ManyToManyRelation, fields.ReverseRelation)):
-            return list(value)
-        return value
+    created_at: datetime
+    updated_at: datetime = None
 
     class Config:
         orm_mode = True
 
 
-class Comment(BaseModel):
-    """
-    Базовая модель комментария
-    """
-    id: int
-    content: str
-    owner_id: int
-    first_name: Optional[str]
-    last_name: Optional[str]  # TODO:  Изменить на модель! !!
-    username: str
-    nearest_ancestor_id: Optional[int]
-    article_id: int
-    state: int  # TODO: перейти на enum
-    answers: Optional[list[dict]]  # Todo проверить типы
-    create_time: datetime
-    update_time: Optional[datetime]
+class ArticleSmall(BaseModel):
+    id: uuid.UUID
+    title: str
+    poster_url: str = None
+    tags: list[ArticleTagItem]
+    state: ArticleState
+    owner: UserSmall
+
+    created_at: datetime
+    updated_at: datetime = None
 
     class Config:
         orm_mode = True
-        extra = 'ignore'
-
-
-class CommentBranch(BaseModel):
-    """
-    Модель ветки комментария
-
-    """
-    ancestor_id: int
-    descendant_id: int
-    nearest_ancestor_id: int
-    article: Article
-    level: int
-    create_time: datetime
-
-
-class Tag(BaseModel):
-    """
-    Базовая модель тега
-
-    """
-    id: int
-    name: str
 
 
 class ArticleCreate(BaseModel):
-    poster_url: Optional[str]
     title: str
     content: str
     state: ArticleState
+    poster_url: str = None
     tags: list[str]
 
 
 class ArticleUpdate(BaseModel):
-    poster_url: Optional[str]
-    title: Optional[str]
-    content: Optional[str]
-    state: Optional[ArticleState]
-    tags: Optional[list[str]]
+    poster_url: str = None
+    title: str = None
+    content: str = None
+    state: ArticleState = None
+    tags: list[str] = None
 
     class Config:
         extra = 'ignore'
