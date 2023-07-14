1bf25a16b7f3eabef3601e7ed0fee1f5bcd1168d
JKearnsl <boyarshinovn@gmail.com>
2023-07-14 21:29:38 +0300

    Добавлен `RepoFactory`

--- src/services/repository/__init__.py
+++ src/services/repository/__init__.py
@@ -1,4 +1,29 @@
-"""Db abstraction layer"""
-from .user import UserRepo, DeletedUserRepo
-from .article import ArticleRepo, CommentRepo, CommentTreeRepo
+from .comment import CommentRepo, CommentTreeRepo
+from .file import FileRepo
 from .notification import NotificationRepo
+from .user import UserRepo
+
+
+class RepoFactory:
+    def __init__(self, session):
+        self._session = session
+
+    @property
+    def user(self) -> UserRepo:
+        return UserRepo(self._session)
+
+    @property
+    def notification(self) -> NotificationRepo:
+        return NotificationRepo(self._session)
+
+    @property
+    def comment(self) -> CommentRepo:
+        return CommentRepo(self._session)
+
+    @property
+    def comment_tree(self) -> CommentTreeRepo:
+        return CommentTreeRepo(self._session)
+
+    @property
+    def file(self) -> FileRepo:
+        return FileRepo(self._session)
