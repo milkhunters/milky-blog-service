0bd1feb4792529a4e74969dd702760312d2080b2
JKearnsl <boyarshinovn@gmail.com>
2023-07-14 21:29:22 +0300

    Переработан `BaseRepo`: теперь  `BaseRepo` -> `BaseRepository`

--- src/services/repository/base.py
+++ src/services/repository/base.py
@@ -1,95 +1,90 @@
-from typing import TypeVar, Generic, List, Optional
+import uuid
+from typing import Generic, Type, TypeVar, Optional
 
-from tortoise.expressions import Q
-from tortoise.fields import CharField
-
-from utils import formators
+from sqlalchemy import insert, update, delete, func, select, text
+from sqlalchemy.ext.asyncio import AsyncSession
 
 T = TypeVar('T')
 
 
-class BaseRepo(Generic[T]):
-    table = T
+class BaseRepository(Generic[T]):
+    table: Type[T]
 
-    async def get(self, fetch_related_fields: Optional[list[str]] = None, **kwargs) -> Optional[T]:
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    async def create(self, **kwargs) -> T:
         """
-        Получает запись по условию
+        Создает запись в БД
 
-        :param fetch_related_fields: список полей, являющихся связными, которые будут загружены
+        :param kwargs:
+        :return:
         """
-        row = await self.table.get_or_none(**kwargs)
-        if row and fetch_related_fields:
-            await row.fetch_related(*fetch_related_fields)
-        return row
+        model = self.table(**kwargs)
+        self._session.add(model)
+        await self._session.commit()
+        return model
 
-    async def search(
-            self,
-            query: Optional[str] = None,
-            fields: Optional[list[str]] = None,
-            limit: int = 40,
+    async def get(self, **kwargs) -> Optional[T]:
+        """
+        Получает запись
+
+        :param kwargs:
+        :return:
+        """
+        return (await self._session.execute(select(self.table).filter_by(**kwargs))).scalars().first()
+
+    async def get_all(
+            self, limit: int = 100,
             offset: int = 0,
-            order_by: Optional[str] = "id",
-            fetch_related_fields: Optional[list[str]] = None,
+            order_by: str = "id",
             **kwargs
-    ) -> Optional[List[T]]:
+    ) -> list[Optional[T]]:
         """
-        :param query: поисковый запрос
-        :param fields: список полей, по которым будет производиться поиск
-        :param limit: максимальное количество возвращаемых записей
-        :param offset: смещение от начала списка
-        :param order_by: поле, по которому будет производиться сортировка
-        :param fetch_related_fields: список полей, которые будут загружены вместе с основной записью
-        :param kwargs: дополнительные условия для фильтрации
+        Получает все записи
 
-        Пример:
-
-        >>> await self.search(query="test", fields=["name", "email"], limit=10, offset=0, order_by="id")
-
-        >>> await self.search(query="test", fields=["name", "email"], limit=10, offset=0, order_by="id", is_active=True)
-
-        >>> await self.search(is_active=True)
-
-        >>> await self.search(first_name="test", last_name="test")
-
-        >>> await self.search()
+        :param limit: лимит 100
+        :param offset: смещение 0
+        :param kwargs: filter by
+        :param order_by: сортировка
+        :return:
         """
-        qs = Q()
-        queries: list[Q] = []
-        kwargs_queries = [Q(**{f"{key}": value}) for key, value in kwargs.items()]
-        if query:
-            queryset = formators.tokenize(query)
+        result = await self._session.execute(
+            select(self.table).filter_by(**kwargs).order_by(text(order_by)).limit(limit).offset(offset)
+        )
+        return result.scalars().all()
 
-            if not fields:
-                fields = [f.model_field_name for f in self.table._meta.fields_map.values() if isinstance(f, CharField)]
+    async def update(self, id: uuid.UUID, **kwargs) -> None:
+        """
+        Обновляет запись
 
-            search_queries = [Q(**{f"{field}__search": " | ".join(queryset)}) for field in fields]
-            icontains_queries = []
-            for query in queryset:
-                icontains_queries += [Q(**{f"{field}__icontains": query}) for field in fields]
+        :param id:
+        :param kwargs:
+        :return:
+        """
+        if kwargs:
+            await self._session.execute(update(self.table).where(self.table.id == id).values(**kwargs))
+            await self._session.commit()
 
-            queries += search_queries + icontains_queries
-        queries += kwargs_queries
+    async def delete(self, id: uuid.UUID) -> None:
+        """
+        Удаляет запись
 
-        for query in queries:
-            qs |= query
-
-        result = await self.table.filter(qs).limit(limit).offset(offset).order_by(order_by)
-        if not result:
-            return None
-
-        if fetch_related_fields:
-            await self.table.fetch_for_list(result, *fetch_related_fields)
-
-        return result  # TODO: протестировать
+        :param id:
+        :return:
+        """
+        await self._session.execute(delete(self.table).where(self.table.id == id))
+        await self._session.commit()
 
     async def count(self, **kwargs) -> int:
-        return await self.table.filter(**kwargs).count()
+        """
+        Возвращает количество записей
 
-    async def insert(self, **kwargs) -> T:
-        return await self.table.create(**kwargs)
+        :param kwargs:
+        :return:
+        """
+        return (await self._session.execute(select(func.count()).where(**kwargs))).scalar()
 
-    async def update(self, id: int, **kwargs) -> None:
-        await self.table.filter(id=id).update(**kwargs)
-
-    async def delete(self, id: int) -> None:
-        await self.table.filter(id=id).delete()
+    @property
+    def session(self) -> AsyncSession:
+        return self._session
