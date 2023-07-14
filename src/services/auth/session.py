50951364d7c55d5c4ae28011e9d7b053f30b3df5
JKearnsl <boyarshinovn@gmail.com>
2023-07-14 21:24:27 +0300

    Некоторые доработки и улучшения

--- src/services/auth/session.py
+++ src/services/auth/session.py
@@ -19,7 +19,7 @@ class SessionManager:
         self._config = config
         self._debug = debug
 
-    def get_session_id(self, req_obj: Request | WebSocket) -> Optional[int]:
+    def get_session_id(self, req_obj: Request | WebSocket) -> str | None:
         """
         Получить идентификатор сессии из куков
 
@@ -27,19 +27,14 @@ class SessionManager:
         :return: session_id
         """
 
-        str_cookie_session_id = req_obj.cookies.get(self.COOKIE_SESSION_KEY)
-        try:
-            cookie_session_id = int(str_cookie_session_id)
-        except (ValueError, TypeError):
-            return None
-        return cookie_session_id
+        return req_obj.cookies.get(self.COOKIE_SESSION_KEY)
 
     async def set_session_id(
             self,
             response: Response,
             refresh_token: str,
-            session_id: int = None
-    ) -> int:
+            session_id: str = None
+    ) -> str:
         """
         Генерирует (если не передано) и устанавливает сессию в redis и в куки
 
@@ -49,27 +44,27 @@ class SessionManager:
         :return: session_id
         """
         if not session_id:
-            session_id = uuid.uuid4().int
+            session_id = uuid.uuid4().hex
         response.set_cookie(
             key=self.COOKIE_SESSION_KEY,
-            value=str(session_id),
+            value=session_id,
             secure=self._config.IS_SECURE_COOKIE,
             httponly=True,
             samesite="strict",
             max_age=self.COOKIE_EXP,
             path=self.COOKIE_PATH
         )
-        await self._redis_client.set(str(session_id), refresh_token, expire=self.REDIS_EXP)
+        await self._redis_client.set(session_id, refresh_token, expire=self.REDIS_EXP)
         return session_id
 
-    async def delete_session_id(self, session_id: int, response: Response) -> None:
+    async def delete_session_id(self, session_id: str, response: Response) -> None:
         """
         Удаляет сессию из куков и из redis
 
         :param session_id
         :param response
         """
-        await self._redis_client.delete(str(session_id))
+        await self._redis_client.delete(session_id)
         response.delete_cookie(
             key=self.COOKIE_SESSION_KEY,
             secure=self._config.IS_SECURE_COOKIE,
@@ -78,14 +73,14 @@ class SessionManager:
             path=self.COOKIE_PATH
         )
 
-    async def is_valid_session(self, session_id: int | str, cookie_refresh_token: str) -> bool:
+    async def is_valid_session(self, session_id: str, cookie_refresh_token: str) -> bool:
         """
         Проверяет валидность сессии
         :param session_id:
         :param cookie_refresh_token:
         :return: True or False
         """
-        refresh_token_from_redis = await self._redis_client.get(str(session_id))
+        refresh_token_from_redis = await self._redis_client.get(session_id)
         if not refresh_token_from_redis:
             return False
         if refresh_token_from_redis != cookie_refresh_token:
