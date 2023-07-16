02c2a2fc935c7be6ca39e6d6694c708224fb40b0
JKearnsl <boyarshinovn@gmail.com>
2023-07-16 23:57:03 +0300

    Переработан контроллер пользователя

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,53 +1,45 @@
-import logging
+import uuid
 
 from fastapi import APIRouter, Depends
+from fastapi import status as http_status
 from fastapi.requests import Request
 from fastapi.responses import Response
 
-from models import schemas
-from config import load_docs
-from dependencies import JWTCookie
-from exceptions.api import APIError
-from services.auth import logout
-from exceptions.models import ErrorAPIResponse
-import views
+from src.dependencies.services import get_services
+from src.models import schemas
+from src.services import ServiceFactory
 
-from services import UserService
+from src.views.user import UserResponse, UserSmallResponse
 
-router = APIRouter(responses={"400": {"model": ErrorAPIResponse}})
-docs = load_docs("user.ini")
+router = APIRouter()
 
 
-@router.get("/current", dependencies=[Depends(JWTCookie())], response_model=views.UserResponse)
-async def get_current_user(request: Request):
-    user_service = UserService()
-
-    user = await user_service.get_user(request.user.id)
-    if not user:
-        raise APIError(api_code=919)
-    return user
+@router.get("/current", response_model=UserResponse, status_code=http_status.HTTP_200_OK)
+async def get_current_user(services: ServiceFactory = Depends(get_services)):
+    return UserResponse(content=await services.user.get_me())
 
 
-@router.get("/{user_id}", response_model=views.UserOutResponse)
-async def get_user(user_id: int):
-    user_service = UserService()
+@router.put("/update", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
+async def update_current_user(data: schemas.UserUpdate, services: ServiceFactory = Depends(get_services)):
+    await services.user.update_me(data)
 
-    user = await user_service.get_user(user_id)
-    if not user:
-        raise APIError(api_code=904)
-    return user
-
-
-@router.post("/update", dependencies=[Depends(JWTCookie())], response_model=views.UserResponse)
-async def update_user(data: schemas.UserUpdate, request: Request):
-    user_service = UserService()
-
-    await user_service.update_user(request.user.id, **data.dict(exclude_unset=True))
+#
+# @router.put("/update/password", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
+# async def update_password():
+#     await services.user.update_password(new_hashed_password, old_hashed_password, new_enc_private_key)
 
 
-@router.delete("/delete", dependencies=[Depends(JWTCookie())])
-async def delete_user(request: Request, response: Response):
-    user_service = UserService()
+@router.get("/{user_id}", response_model=UserSmallResponse, status_code=http_status.HTTP_200_OK)
+async def get_user(user_id: uuid.UUID, services: ServiceFactory = Depends(get_services)):
+    return UserSmallResponse(content=await services.user.get_user(user_id))
 
-    await logout(request, response)
-    await user_service.delete_user(request.user.id)
+
+@router.delete("/delete", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
+async def delete_current_user(
+        password: str,
+        request: Request,
+        response: Response,
+        services: ServiceFactory = Depends(get_services)
+):
+    await services.user.delete_me(password)
+    await services.auth.logout(request, response)
