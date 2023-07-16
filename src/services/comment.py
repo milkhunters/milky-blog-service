e95403c1210b234f8487a9513f6fa868be5506f1
JKearnsl <boyarshinovn@gmail.com>
2023-07-16 21:20:06 +0300

    Теперь есть возможность создавать комментарий

--- src/services/comment.py
+++ src/services/comment.py
@@ -1,50 +1,89 @@
-from typing import Optional
+"""
+    Сервис для работы с комментариями
 
-from models.state import CommentState
-from models.state import NotificationTypes
-from services.notification import NotificationService
-from services.repository import CommentRepo
-from services.repository import CommentTreeRepo
+    Используется подход ...
 
 
-class CommentService:
+"""
+import uuid
+
+from src.models import schemas
+from src.models.auth import BaseUser
+from src.models.role import Role, MainRole as M, AdditionalRole as A
+from src.models.state import CommentState
+from src.models.state import NotificationType
+
+from src import exceptions
+from src.services.auth import role_filter
+from src.services.repository import CommentRepo, ArticleRepo
+from src.services.repository import CommentTreeRepo
+from src.services.repository import NotificationRepo
+
+
+class CommentApplicationService:
     def __init__(
             self,
-            comment_repo: CommentRepo = CommentRepo(),
-            comment_tree_repo: CommentTreeRepo = CommentTreeRepo()
+            current_user: BaseUser,
+            comment_repo: CommentRepo,
+            comment_tree_repo: CommentTreeRepo,
+            notify_repo: NotificationRepo,
+            article_repo: ArticleRepo
     ):
+        self._current_user = current_user
         self._repo = comment_repo
         self._tree_repo = comment_tree_repo
+        self._notification_repo = notify_repo
+        self._article_repo = article_repo
 
-    async def add_comment(self, article_id: int, content: str, owner_id: int, parent_id: int = 0):
+    @role_filter(min_role=Role(M.USER, A.ONE))
+    async def add_comment(
+            self,
+            article_id: uuid.UUID,
+            data: schemas.CommentCreate,
+            parent_id: uuid.UUID = None
+    ) -> schemas.Comment:
         """
         Добавить комментарий к публикации
 
-        :param article_id: ид публикации
-        :param content: текст комментария
-        :param owner_id: автор комментария
-        :param parent_id: родитель, если это ответ (ид комментария)
+        :param article_id: идентификатор публикации
+        :param data: данные комментария
+        :param parent_id: идентификатор родительского комментария
         :return:
 
-        # TODO: переписать
-
         """
-        new_comment = await self._repo.insert(
-            content=content,
-            owner_id=owner_id,  # TODO: изменено
+
+        article = await self._article_repo.get(id=article_id)
+        if article is None:
+            raise exceptions.NotFound("Публикация не найдена")
+
+        parent = None
+        parent_level = -1
+        if parent_id is not None:
+            parent = await self._repo.get(id=parent_id)
+            if parent is None:
+                raise exceptions.NotFound("Родительский комментарий не найден")
+
+            parent_as_node = await self._tree_repo.get(ancestor_id=parent_id, descendant_id=parent_id)
+            if parent_as_node is None:
+                raise exceptions.NotFound("Родительский комментарий не связан с публикацией")
+
+            if parent_as_node.article_id != article_id:
+                raise exceptions.BadRequest(f"Комментарий не принадлежит публикации с id:{article_id}")
+
+            parent_level = parent_as_node.level
+
+        new_comment = await self._repo.create(
+            content=data.content,
+            owner_id=self._current_user.id,
         )
 
-        parent_level = -1
-        if parent_id != 0:
-            parent = await self._repo.get(id=parent_id) # Допустить вложенность в дерево комментариев, чтобы получить уровень вложенности
-            parent_level = parent.level
-            parent_owner_id = (await self.get_comment(parent_id))["owner_id"]
-            if parent_owner_id != new_comment.owner_id:
-                await NotificationService().create(
-                    user_id=parent_owner_id,
-                    notification_type=NotificationTypes.comment_answer,
-                    data=new_comment.id
-                )
+        if parent and parent.owner_id != new_comment.owner_id:
+            await self._notification_repo.create(
+                owner_id=parent.owner_id,
+                type_id=NotificationType.COMMENT_ANSWER,
+                content_id=new_comment.id,
+                content=f"{new_comment.owner.username} ответил на ваш комментарий"
+            )
 
         await self._tree_repo.create_branch(
             parent_id=parent_id,
@@ -52,22 +91,24 @@ class CommentService:
             article_id=article_id,
             parent_level=parent_level
         )
-        return new_comment
+        return schemas.Comment.model_validate(new_comment)
 
-    async def get_comment(self, comment_id: int) -> Optional[dict]:
+    @role_filter(min_role=Role(M.GUEST, A.ONE))
+    async def get_comment(self, comment_id: uuid.UUID) -> schemas.Comment:
         """
-        Получить комментарий и его ветку (ответы)
-
-        :param comment_id:
-        :return:
+        Получить комментарий
         """
-        raw_data = await self._db.get_raw_comment(comment_id)
-        self._normalize(raw_data)
-        if raw_data:
-            return raw_data[0]
-        return None
+        # check if exists
+        comment = await self._repo.get(id=comment_id)
+        if comment is None:
+            raise exceptions.NotFound(f"Комментарий c id:{comment_id} не найден")
 
-    async def get_comments(self, article_id: int) -> list[dict]:
+        if comment.state == CommentState.DELETED and self._current_user.role < Role(M.ADMIN, A.ONE):
+            raise exceptions.NotFound(f"Комментарий c id:{comment_id} не найден")
+
+        return schemas.Comment.model_validate(comment)
+
+    async def get_comments(self, article_id: uuid.UUID) -> list[dict]:
         """
         Получить все комментарии публикации
 
@@ -78,7 +119,7 @@ class CommentService:
         self._normalize(raw_data)
         return raw_data
 
-    async def delete(self, comment_id):
+    async def delete(self, comment_id: uuid.UUID) -> None:
         """
         Удалить комментарий
         (Изменить состояние - deleted)
@@ -86,6 +127,14 @@ class CommentService:
         :param comment_id:
         :return:
         """
+        # comment = await cs.get_comment(id)
+        # if not comment:
+        #     raise APIError(919)
+        # if comment["state"] == CommentState.deleted:
+        #     raise APIError(919)
+        # if comment["owner_id"] != request.user.id:
+        #     if request.user.role_id < 21:
+        #         raise APIError(909)
         await self._repo.update(id=comment_id, state=CommentState.deleted)
 
     async def delete_all_comments(self, article_id: int) -> None:
@@ -99,6 +148,21 @@ class CommentService:
         comment_ids = await self._repo.get_id_comments_in_article(article_id)
         await self._tree_repo.delete_all_branches(article_id)
         await self._repo.delete_comments(comment_ids)
+
+    async def update_comment(self, comment_id: uuid.UUID, data: schemas.CommentUpdate) -> None:
+        """
+        Изменить комментарий
+
+        :param comment_id:
+        :param data:
+        :return:
+        """
+        # comment = await cs.get_comment(id)
+        # if not comment or (comment["state"] == CommentState.deleted):
+        #     raise APIError(919)
+        # if comment["owner_id"] != request.user.id:
+        #     raise APIError(909)
+        await self._repo.update(id=comment_id, content=data.content)
 
     @staticmethod
     def _normalize(raw_data: list[dict]) -> None:
@@ -118,7 +182,7 @@ class CommentService:
             # Берем последний эл-т
             row = raw_data[i]
             # Если комментарий удален
-            if row["state"] == CommentState.deleted:
+            if row["state"] == CommentState.DELETED:
                 row["content"] = "Комментарий удален"
                 row["owner_id"] = 0
                 row["first_name"] = ""
