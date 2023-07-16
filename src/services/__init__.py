bfc7422905d218403dd3903e924091284ce97ff5
JKearnsl <boyarshinovn@gmail.com>
2023-07-17 01:13:30 +0300

    Добавлен `EmailService`

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,9 +1,12 @@
+import aio_pika
+
 from src.models.auth import BaseUser
 from . import repository
 from . import auth
 from .article import ArticleApplicationService
 from .auth import AuthApplicationService
 from .comment import CommentApplicationService
+from .email import EmailService
 from .notification import NotificationApplicationService
 from .user import UserApplicationService
 
@@ -16,11 +19,13 @@ class ServiceFactory:
             current_user: BaseUser,
             config,
             redis_client,
+            rmq: aio_pika.RobustConnection,
     ):
         self._repo = repo_factory
         self._current_user = current_user
         self._config = config
         self._redis_client = redis_client
+        self._rmq = rmq
 
     @property
     def user(self) -> UserApplicationService:
@@ -32,7 +37,9 @@ class ServiceFactory:
             self._current_user,
             jwt_manager=auth.JWTManager(config=self._config),
             session_manager=auth.SessionManager(redis_client=self._redis_client, config=self._config),
-            user_repo=self._repo.user
+            user_repo=self._repo.user,
+            redis_client=self._redis_client,
+            email_service=self.email,
         )
 
     @property
@@ -58,3 +65,7 @@ class ServiceFactory:
     @property
     def notification(self) -> NotificationApplicationService:
         return NotificationApplicationService(self._current_user, notify_repo=self._repo.notification)
+
+    @property
+    def email(self) -> EmailService:
+        return EmailService(config=self._config.EMAIL, rmq=self._rmq)
