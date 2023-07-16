da6f065ac49d2b893a5db929fbf9a5f95933d31f
JKearnsl <boyarshinovn@gmail.com>
2023-07-17 01:11:23 +0300

    Добавлена возможность отправки почты

--- src/services/email.py
+++ src/services/email.py
@@ -1,33 +1,35 @@
-import logging
+import aio_pika
 
-from config import load_config
-from utils.rabbitmq import RabbitMQ
-
-config = load_config()
+from src.config import Email
 
 
 class EmailService:
-    CONTENT_TYPE = "text/html"
-    FROM_NAME = "MILKY-TEAM"
+    def __init__(self, rmq: aio_pika.RobustConnection, config: Email):
+        self._rmq = rmq
+        self._config = config
 
-    async def send_mail(self, rabbitmq: RabbitMQ, email: str, subject: str, message: str):
+    async def send_mail(self, email: str, subject: str, message: str, content_type: str = "text/html"):
         """
         Отправляет сообщение на почту
 
-        :param email: почта пользователя.
+        :param email: почта получателя.
         :param subject: тема сообщения
         :param message: текст сообщения
-        :param rabbitmq: клиент rabbitmq
-
+        :param content_type: тип контента
         """
 
-        await rabbitmq.send(
-            body=message,
-            headers={
-                "To": email,
-                "Subject": subject,
-                "FromName": self.FROM_NAME
-            },
-            routing_key=config.base.amqp.queue,
-            content_type=self.CONTENT_TYPE
+        channel = await self._rmq.channel()
+        queue = await channel.declare_queue(self._config.RabbitMQ.QUEUE, durable=True)
+
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                headers={
+                    "To": email,
+                    "Subject": subject,
+                    "FromName": self._config.FROM_NAME
+                },
+                body=message.encode(),
+                content_type=content_type
+            ),
+            routing_key=queue.name,
         )
