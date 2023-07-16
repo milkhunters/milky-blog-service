32539fa28ffdb9be7eb431a8dcb1b6be56edbeef
JKearnsl <boyarshinovn@gmail.com>
2023-07-16 23:56:43 +0300

    мелкие правки

--- src/controllers/blog/__init__.py
+++ src/controllers/blog/__init__.py
@@ -1,17 +1,9 @@
-from fastapi import APIRouter, Depends
-
-from dependencies import JWTCookie, RoleFilter
-from models.role import Role
-from models.role import MainRole as M
-from models.role import AdditionalRole as A
-
-
-from exceptions.models import ErrorAPIResponse
+from fastapi import APIRouter
 
 from . import article
 from . import comments
 
-router = APIRouter(responses={"400": {"model": ErrorAPIResponse}})
+router = APIRouter()
 
 router.include_router(article.router, prefix="/article")
 router.include_router(comments.router, prefix="/comment")
