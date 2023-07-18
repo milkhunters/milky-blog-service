93dfcf65ff2b096fc4b07a833d70b29c53b2f605
JKearnsl <boyarshinovn@gmail.com>
2023-07-18 22:39:13 +0300

    Добавлено описание к маршрутам

--- src/controllers/blog/comments.py
+++ src/controllers/blog/comments.py
@@ -18,16 +18,33 @@ async def create_comment(
         parent_id: uuid.UUID = None,
         service: ServiceFactory = Depends(get_services)
 ):
+    """
+    Создать комментарий
+
+    Минимальная роль: USER.ONE
+
+    Состояние: ACTIVE
+    """
     return CommentResponse(content=await service.comment.add_comment(article_id, data, parent_id))
 
 
 @router.get("/list", response_model=CommentsResponse, status_code=http_status.HTTP_200_OK)
 async def get_comments(article_id: uuid.UUID, service: ServiceFactory = Depends(get_services)):
+    """
+    Получить список комментариев публикации
+
+    Минимальная роль: GUEST.ONE
+    """
     return CommentsResponse(content=await service.comment.get_comments(article_id))
 
 
 @router.get("/get", response_model=CommentResponse, status_code=http_status.HTTP_200_OK)
 async def get_comment(comment_id: uuid.UUID, service: ServiceFactory = Depends(get_services)):
+    """
+    Получить комментарий
+
+    Минимальная роль: GUEST.ONE
+    """
     return CommentResponse(content=await service.comment.get_comment(comment_id))
 
 
@@ -37,18 +54,37 @@ async def update_comment(
         data: schemas.CommentUpdate,
         service: ServiceFactory = Depends(get_services)
 ):
+    """
+    Обновить комментарий
+
+    Минимальная роль: USER.ONE
+
+    Состояние: ACTIVE
+
+    Пользователь с ролью ADMIN.ONE может обновить чужой комментарий
+    """
     await service.comment.update_comment(comment_id, data)
 
 
 @router.delete("/delete/{comment_id}", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
 async def delete_comment(comment_id: uuid.UUID, service: ServiceFactory = Depends(get_services)):
+    """
+    Удалить комментарий
+
+    Минимальная роль: USER.ONE
+
+    Состояние: ACTIVE
+
+    Пользователь с ролью ADMIN.ONE может удалить чужой комментарий
+    """
     await service.comment.delete_comment(comment_id)
 
 
 @router.delete("/delete_all/{article_id}", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
 async def delete_all_comments(article_id: uuid.UUID, service: ServiceFactory = Depends(get_services)):
     """
-    Администратор может удалить все комментарии статьи
+    Удалить все комментарии статьи
 
+    Только Администратор(>=ADMIN.ONE) может удалить все комментарии статьи
     """
     await service.comment.delete_all_comments(article_id)
