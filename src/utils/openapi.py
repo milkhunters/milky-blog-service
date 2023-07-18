12afdda6d147113daec2ad6df8c345db435096ac
JKearnsl <boyarshinovn@gmail.com>
2023-07-18 19:13:39 +0300

    Обновлена спецификация `openapi`: лого

--- src/utils/openapi.py
+++ src/utils/openapi.py
@@ -1,9 +1,9 @@
 from fastapi.openapi.utils import get_openapi
 
 
-def custom_openapi(app):
+def custom_openapi(app, logo_url: str = None):
     if not app.openapi_schema:
-        app.openapi_schema = get_openapi(
+        openapi_schema = get_openapi(
             title=app.title,
             version=app.version,
             openapi_version=app.openapi_version,
@@ -15,10 +15,15 @@ def custom_openapi(app):
             tags=app.openapi_tags,
             servers=app.servers,
         )
-        for _, method_item in app.openapi_schema.get('paths').items():
+        for _, method_item in openapi_schema.get('paths').items():
             for _, param in method_item.items():
                 responses = param.get('responses')
                 # remove 422 response, also can remove other status code
                 if '422' in responses:
                     del responses['422']
+
+        openapi_schema["info"]["x-logo"] = {
+            "url": logo_url
+        }
+        app.openapi_schema = openapi_schema
     return app.openapi_schema
