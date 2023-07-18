759e06b7354a1983745b9c2cb1b5d414d996430f
JKearnsl <boyarshinovn@gmail.com>
2023-07-18 22:39:19 +0300

    Добавлено описание к маршрутам

--- src/controllers/blog/article.py
+++ src/controllers/blog/article.py
@@ -23,6 +23,14 @@ async def get_articles(
         owner_id: uuid.UUID = None,
         services: ServiceFactory = Depends(get_services)
 ):
+    """
+    Получить список статей
+
+    Минимальная роль: GUEST.ONE
+
+    Если указан owner_id, то возвращаются только статьи этого пользователя, причем пользователь с ролью ADMIN.ONE может
+    просматривать чужие публикации.
+    """
     return ArticlesResponse(
         content=await services.article.get_articles(page, per_page, order_by, query, state, owner_id)
     )
@@ -30,11 +38,25 @@ async def get_articles(
 
 @router.post("/create", response_model=ArticleResponse, status_code=http_status.HTTP_201_CREATED)
 async def create_article(article: schemas.ArticleCreate, services: ServiceFactory = Depends(get_services)):
+    """
+    Создать статью
+
+    Минимальная роль: MODER.ONE
+
+    Состояние: ACTIVE
+
+    Максимальный размер статьи - 32000 символов
+    """
     return ArticleResponse(content=await services.article.create_article(article))
 
 
 @router.get("/{article_id}", response_model=ArticleResponse, status_code=http_status.HTTP_200_OK)
 async def get_article(article_id: uuid.UUID, services: ServiceFactory = Depends(get_services)):
+    """
+    Получить статью по id
+
+    Минимальная роль: GUEST.ONE
+    """
     return ArticleResponse(content=await services.article.get_article(article_id))
 
 
@@ -44,9 +66,27 @@ async def update_article(
         data: schemas.ArticleUpdate,
         services: ServiceFactory = Depends(get_services)
 ):
+    """
+    Обновить статью по id
+
+    Минимальная роль: MODER.ONE
+
+    Состояние: ACTIVE
+
+    Причем пользователь с ролью ADMIN.ONE может редактировать чужие публикации.
+    """
     await services.article.update_article(article_id, data)
 
 
 @router.delete("/delete/{article_id}", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
 async def delete_article(article_id: uuid.UUID, services: ServiceFactory = Depends(get_services)):
+    """
+    Удалить статью по id
+
+    Минимальная роль: MODER.ONE
+
+    Состояние: ACTIVE
+
+    Причем пользователь с ролью ADMIN.ONE может удалять чужие публикации.
+    """
     await services.article.delete_article(article_id)
