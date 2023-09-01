942e71e7fac7d3c500622fded7364cdcfd03e11f
JKearnsl <boyarshinovn@gmail.com>
2023-09-01 11:22:51 +0300

    Обновлена зависимость

--- src/dependencies/services.py
+++ src/dependencies/services.py
@@ -1,6 +1,5 @@
 from fastapi import Depends
 from fastapi.requests import Request
-from fastapi.websockets import WebSocket
 
 from src.dependencies.repos import get_repos
 from src.services import ServiceFactory
@@ -8,22 +7,14 @@ from src.services.repository import RepoFactory
 
 
 async def get_services(
-        request: Request = None,
-        websocket: WebSocket = None,
+        request: Request,
         repos: RepoFactory = Depends(get_repos)
 ) -> ServiceFactory:
-    if request:
-        app = request.app
-        scope = request.scope
-    else:
-        app = websocket.app
-        scope = websocket.scope
+    global_scope = request.app.state
+    local_scope = request.scope
 
     yield ServiceFactory(
         repos,
-        current_user=scope.get("user"),
-        config=request.app.state.config,
-        redis_client=app.state.redis,
-        rmq=app.state.rmq,
-        file_storage=app.state.file_storage,
+        current_user=local_scope.get("user"),
+        config=global_scope.config,
     )
