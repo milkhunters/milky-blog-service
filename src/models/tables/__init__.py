d5c816f54b83fec86afaa5498d99307433432cb4
JKearnsl <boyarshinovn@gmail.com>
2023-09-01 11:24:20 +0300

    Очищены старые зависимости

--- src/models/tables/__init__.py
+++ src/models/tables/__init__.py
@@ -1,10 +1,8 @@
 from src.db import Base
 
-from .user import User
 from .article import ArticleTag
 from .article import Article
 from .tag import Tag
 from .comment import Comment
 from .comment import CommentTree
 from .notification import Notification
-from .file import File
