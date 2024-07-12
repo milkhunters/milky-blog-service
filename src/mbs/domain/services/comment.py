b415445bbc181f0569dbe030a26ec4d09f8d9399
JKearnsl <boyarshinovn@gmail.com>
2024-07-12 14:22:12 +0300

    added comment service

---
+++ src/mbs/domain/services/comment.py
@@ -0,0 +1,32 @@
+import uuid
+import datetime
+
+from mbs.domain.models import Comment, CommentState, UserId
+
+
+class CommentService:
+
+    def create_comment(self, content: str, author_id: UserId):
+        return Comment(
+            id=uuid.uuid4(),
+            content=content,
+            author_id=author_id,
+            state=CommentState.PUBLISHED,
+            created_at=datetime.datetime.now(datetime.UTC),
+            updated_at=None
+        )
+
+    def update_comment(
+            self,
+            comment: Comment,
+            content: str,
+            state: CommentState,
+    ):
+        return Comment(
+            id=comment.id,
+            content=content,
+            author_id=comment.author_id,
+            state=state,
+            created_at=comment.created_at,
+            updated_at=datetime.datetime.now(datetime.UTC)
+        )
