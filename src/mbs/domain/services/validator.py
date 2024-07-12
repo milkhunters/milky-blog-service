7190de01e064eae31fdf272066b9b1e299866371
JKearnsl <boyarshinovn@gmail.com>
2024-07-12 14:22:19 +0300

    added validator service

---
+++ src/mbs/domain/services/validator.py
@@ -0,0 +1,51 @@
+import uuid
+import datetime
+
+from mbs.domain.exceptions import ValidationError
+from mbs.domain.models import Article
+from mbs.domain.models.article_state import ArticleState
+
+
+class ValidatorService:
+
+    def __init__(self):
+        self.article_content_max_length = 32000
+        self.article_title_max_length = 255
+        self.article_title_min_length = 8
+        self.article_content_min_length = 32
+
+        self.comment_content_max_length = 1000
+        self.comment_content_min_length = 8
+
+        self.tag_title_max_length = 32
+        self.tag_title_min_length = 3
+
+
+
+    def validate_article_title(self, title: str):
+        if len(title) < self.article_title_min_length:
+            raise ValidationError(f"Заголовок не может содержать меньше {self.article_title_min_length} символов")
+        if len(title) > self.article_title_max_length:
+            raise ValidationError(f"Заголовок не может содержать больше {self.article_title_max_length} символов")
+        return title
+
+    def validate_article_content(self, content: str):
+        if len(content) < self.article_content_min_length:
+            raise ValidationError(f"Публикация не может содержать меньше {self.article_content_min_length} символов")
+        if len(content) > self.article_content_max_length:
+            raise ValidationError(f"Публикация не может содержать больше {self.article_content_max_length} символов")
+        return content
+
+    def validate_comment_content(self, content: str):
+        if len(content) < self.comment_content_min_length:
+            raise ValidationError(f"Комментарий не может содержать меньше {self.comment_content_min_length} символов")
+        if len(content) > self.comment_content_max_length:
+            raise ValidationError(f"Комментарий не может содержать больше {self.comment_content_max_length} символов")
+        return content
+
+    def validate_tag_title(self, title: str):
+        if len(title) < self.tag_title_min_length:
+            raise ValidationError(f"Тег не может содержать меньше {self.tag_title_min_length} символов")
+        if len(title) > self.tag_title_max_length:
+            raise ValidationError(f"Тег не может содержать больше {self.tag_title_max_length} символов")
+        return title
