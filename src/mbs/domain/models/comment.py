aa3009d0246540342641196e545ddfcadab0adde
JKearnsl <boyarshinovn@gmail.com>
2024-07-16 19:45:47 +0300

    added article_id and parent_id

--- src/mbs/domain/models/comment.py
+++ src/mbs/domain/models/comment.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 
+from mbs.domain.models import UserId, ArticleId
 from mbs.domain.models.comment_state import CommentState
-from mbs.domain.models.user_id import UserId
 
 type CommentId = UUID
 
@@ -13,6 +13,8 @@ class Comment(BaseModel):
     id: CommentId
     content: str
     author_id: UserId
+    article_id: ArticleId
+    parent_id: CommentId | None
     state: CommentState
 
     created_at: datetime
