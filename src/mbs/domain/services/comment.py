ee1fed744bd2cb6c2a07a20116070647eb72c5d9
JKearnsl <boyarshinovn@gmail.com>
2024-07-20 02:07:31 +0300

    update: added comment article/parent id

--- src/mbs/domain/services/comment.py
+++ src/mbs/domain/services/comment.py
@@ -1,16 +1,18 @@
 import uuid
 import datetime
 
-from mbs.domain.models import Comment, CommentState, UserId
+from mbs.domain.models import Comment, CommentState, UserId, ArticleId, CommentId
 
 
 class CommentService:
 
-    def create_comment(self, content: str, author_id: UserId):
+    def create_comment(self, content: str, author_id: UserId, article_id: ArticleId, parent_id: CommentId | None):
         return Comment(
             id=uuid.uuid4(),
             content=content,
             author_id=author_id,
+            article_id=article_id,
+            parent_id=parent_id,
             state=CommentState.PUBLISHED,
             created_at=datetime.datetime.now(datetime.UTC),
             updated_at=None
