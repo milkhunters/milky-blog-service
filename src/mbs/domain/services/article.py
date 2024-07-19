b72ef82d058c5a43ff8a31edb561a9d383fc9080
JKearnsl <boyarshinovn@gmail.com>
2024-07-20 02:06:50 +0300

    update: added tags and likes

--- src/mbs/domain/services/article.py
+++ src/mbs/domain/services/article.py
@@ -6,7 +6,15 @@ from mbs.domain.models import Article, ArticleState, FileId, UserId
 
 class ArticleService:
 
-    def create_article(self, title: str, content: str, state: ArticleState, author_id: UserId, poster: FileId = None):
+    def create_article(
+            self,
+            title: str,
+            content: str,
+            state: ArticleState,
+            author_id: UserId,
+            tags: list[str],
+            poster: FileId = None
+    ):
         return Article(
             id=uuid.uuid4(),
             title=title,
@@ -14,6 +22,8 @@ class ArticleService:
             content=content,
             state=state,
             views=0,
+            likes=0,
+            tags=tags,
             author_id=author_id,
             created_at=datetime.datetime.now(datetime.UTC),
             updated_at=None
@@ -25,8 +35,10 @@ class ArticleService:
             title: str,
             content: str,
             state: ArticleState,
+            poster: FileId | None,
             views: int,
-            poster: FileId = None
+            likes: int,
+            tags: list[str]
     ):
         return Article(
             id=article.id,
@@ -35,6 +47,8 @@ class ArticleService:
             content=content,
             state=state,
             views=views,
+            likes=likes,
+            tags=tags,
             author_id=article.author_id,
             created_at=article.created_at,
             updated_at=datetime.datetime.now(datetime.UTC)
