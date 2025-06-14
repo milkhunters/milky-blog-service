17074a7d27a59e78bcbae7dee2e916c967dfd8f2
JKearnsl <boyarshinovn@gmail.com>
2025-06-14 23:51:55 +0300

    add not auth err

--- src/domain/error.rs
+++ src/domain/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
 pub enum DomainError {
+    #[error("you must be authenticated to perform this action")]
+    NotAuth,
     #[error("access denied")]
     Access,
     #[error("validation: {0}")]
