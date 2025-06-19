b2c8c5c40f05ae0ad055902a833498e5efd79504
JKearnsl <boyarshinovn@gmail.com>
2025-06-19 21:33:14 +0300

    add increment_article_views

--- src/application/common/article_gateway.rs
+++ src/application/common/article_gateway.rs
@@ -30,6 +30,7 @@ pub trait ArticleReader {
 #[async_trait]
 pub trait ArticleWriter {
     async fn save_article(&self, article: &Article) -> Result<(), ArticleGatewayError>;
+    async fn increment_article_views(&self, id: &ArticleId) -> Result<(), ArticleGatewayError>;
 }
 
 #[async_trait]
