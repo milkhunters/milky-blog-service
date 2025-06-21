6b67b04dd1b05f540c31b59029ec97463907f231
JKearnsl <boyarshinovn@gmail.com>
2025-06-21 22:49:55 +0300

    optimize

--- src/application/common/id_provider.rs
+++ src/application/common/id_provider.rs
@@ -1,6 +1,9 @@
-use crate::domain::models::permissions::Permission;
-use crate::domain::models::user_id::UserId;
-use crate::domain::models::user_state::UserState;
+use crate::domain::models::{
+    permissions::Permission,
+    user_id::UserId,
+    user_state::UserState
+};
+
 
 pub trait IdProvider {
     fn user_id(&self) -> &UserId;
