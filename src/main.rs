da5a5b2b968fab9290aecf12a200cd94ff85db21
JKearnsl <boyarshinovn@gmail.com>
2025-06-24 23:55:21 +0300

    add basic app config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use crate::domain::models::permissions::Permission;
+
 mod adapters;
 mod application;
 mod domain;
 mod presentation;
+mod ioc;
 
+pub struct AppConfig {
+    pub guest_permissions: Arc<RwLock<Vec<Permission>>>,
+    pub jwt_verify_key: [u8; 32]
+}
 
 #[actix_web::main]
 async fn main() {
