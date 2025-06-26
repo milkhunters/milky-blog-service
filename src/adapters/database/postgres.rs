6e1deac18c60988466c3ae6c50048c72f4d7b932
JKearnsl <boyarshinovn@gmail.com>
2025-06-27 00:19:36 +0300

    replace user -> username param

--- src/adapters/database/postgres.rs
+++ src/adapters/database/postgres.rs
@@ -9,14 +9,14 @@ pub async fn new_pool(
     host: &str,
     port: u16,
     database: &str,
-    user: &str,
+    username: &str,
     password: &str
 ) -> Result<sqlx::PgPool, sqlx::Error> {
     let options = sqlx::postgres::PgConnectOptions::new()
         .host(host)
         .port(port)
         .database(database)
-        .username(user)
+        .username(username)
         .password(password)
         .application_name(env!("CARGO_PKG_NAME"))
         .ssl_mode(sqlx::postgres::PgSslMode::Disable);
