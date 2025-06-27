03a6c74e5fa2ebbb46d895ac9788e335c7b5bb77
JKearnsl <boyarshinovn@gmail.com>
2025-06-27 18:19:23 +0300

    add error builder

--- src/presentation/rest/error.rs
+++ src/presentation/rest/error.rs
@@ -131,3 +131,72 @@ impl From<TokenError> for HttpError {
         HttpError { http_status, inner: HttpErrorModel { status: app_status, details } }
     }
 }
+
+impl HttpErrorModel {
+    pub fn not_found(field: String) -> Self {
+        HttpErrorModel {
+            status: "NOT_FOUND".into(),
+            details: vec![HttpErrorDetail {
+                field,
+                reason: "NOT_FOUND".into(),
+                attrs: vec![],
+            }],
+        }
+    }
+    
+    pub fn access_denied() -> Self {
+        HttpErrorModel {
+            status: "ACCESS_DENIED".into(),
+            details: vec![],
+        }
+    }
+    
+    pub fn token_invalid(msg: String) -> Self {
+        HttpErrorModel {
+            status: "TOKEN_INVALID".into(),
+            details: vec![HttpErrorDetail {
+                field: "jwt".into(),
+                reason: "TOKEN_INVALID".into(),
+                attrs: vec![Value::String(msg)],
+            }],
+        }
+    }
+    
+    pub fn token_expired() -> Self {
+        HttpErrorModel {
+            status: "TOKEN_EXPIRED".into(),
+            details: vec![],
+        }
+    }
+    
+    pub fn validation(fields: Vec<(String, ValidationError)>) -> Self {
+        HttpErrorModel {
+            status: "VALIDATION".into(),
+            details: fields.into_iter().map(|(field, reason)| {
+                let (attrs, reason) = match reason {
+                    ValidationError::InvalidEmpty => (vec![], "INVALID_EMPTY".into()),
+                    ValidationError::InvalidRange(min, max) => (
+                        vec![Value::Number(min.into()), Value::Number(max.into())],
+                        "INVALID_RANGE".into(),
+                    ),
+                    ValidationError::InvalidRegex(re_str) => (
+                        vec![Value::String(re_str)],
+                        "INVALID_REGEX".to_string(),
+                    ),
+                };
+                HttpErrorDetail { field, reason, attrs }
+            }).collect(),
+        }
+    }
+    
+    pub fn critical(msg: String) -> Self {
+        HttpErrorModel {
+            status: "CRITICAL".into(),
+            details: vec![HttpErrorDetail {
+                field: "app".into(),
+                reason: "CRITICAL".into(),
+                attrs: vec![Value::String(msg)],
+            }],
+        }
+    }
+}
